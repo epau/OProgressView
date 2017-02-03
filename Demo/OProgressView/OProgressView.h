c797af5980056ab3a4a2a4b961acc0e8d904aec0
epau <edpaulosky@gmail.com>
2017-02-03 11:20:48 -0500

    Creates framework

---
+++ Demo/OProgressView/OProgressView.h
@@ -0,0 +1,19 @@
+//
+//  OProgressView.h
+//  OProgressView
+//
+//  Created by Edward Paulosky on 2/3/17.
+//  Copyright © 2017 epau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OProgressView.
+FOUNDATION_EXPORT double OProgressViewVersionNumber;
+
+//! Project version string for OProgressView.
+FOUNDATION_EXPORT const unsigned char OProgressViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OProgressView/PublicHeader.h>
+
+
